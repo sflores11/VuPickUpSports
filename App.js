ebb09ae52d7b9a5ed604cdaa40f45fbbc6cf78be
JustinSearcy <justin91924@gmail.com>
2021-04-12 20:17:51 -0400

    Added basic location selection screen

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+class LocationScreen extends Component{
+
+  render(){
+    //You can't add styling to the react buttons so those spacing views are used instead
+    return(
+      <View>
+        <Text style={styles.title}>West Campus</Text>
+        <Button title="Soccer Fields"></Button>
+          <View style={styles.spacing}></View> 
+        <Button title="St. Mary's Gym"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Tennis Courts"></Button>
+        <View style={styles.spacing}></View>
+        <Button title="Volleyball Courts"></Button>
+        <View style={styles.spacing}></View>
+        <Button title="Basketball Courts"></Button>
+        <Text style={styles.title}>South Campus</Text>
+        <Button title="Pike Field"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Volleyball Courts"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Basketball Courts"></Button>
+        <Text style={styles.title}>Main Campus</Text>
+        <Button title="Alumni Gym"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Austin Field"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Tennis Courts"></Button>
+          <View style={styles.spacing}></View>
+        <Button title="Basketball Courts"></Button>
+      </View>
+    );
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LocationScreen></LocationScreen>
     </View>
   );
 }
@@ -18,4 +52,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  title: {
+    fontSize: 30
+  },
+
+  spacing:{
+    width: 5,
+    height: 5
+  }
 });
